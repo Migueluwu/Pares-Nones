e73258bcd2968c7761419d2d601a98dd3a04db08
Miguel <miguelmartinfernandez94@gmail.com>
2021-11-02 12:23:37 +0100

    hace la suma y se calcula quien gana

--- src/main/java/com/mycompany/pares_nones/Manual.java
+++ src/main/java/com/mycompany/pares_nones/Manual.java
@@ -21,6 +21,7 @@ public class Manual {
        int eleccion;
        int numero1;
        int numero2;
+       int suma;
        do{ 
         System.out.println("El jugador 1 tiene que elegir pares o nones");
         System.out.println("Escribe 1 para elegir pares y 2 para elegir nones");
@@ -34,6 +35,14 @@ public class Manual {
         System.out.println("Jugador 2, escribe del 0 10 cuantos dedos enseñas:");
         numero2=entradaTeclado.nextInt();
        }while(numero2>10||numero2<0);
+        suma=numero1+numero2;
+        if(suma%2==0 && eleccion==1){
+            System.out.println("gana el jugador 1");
+        }else if(suma%2!=0 && eleccion==2){
+            System.out.println("gana el jugador 1");
+        }else{
+            System.out.println("gana el jugador 2");
+        }
     }
     
 }
